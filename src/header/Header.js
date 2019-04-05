6f486f54c08b6784a37403d3205f33a4c4a6d1a3
Patiopatio <buczkowska.p@gmail.com>
2019-04-05 20:03:07 +0200

    added test component

---
+++ src/header/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import { Button } from 'reactstrap';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <h1>test 2</h1>
+
+      </div>
+    )
+  }
+}
+
+export default Header
